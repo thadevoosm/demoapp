7cdd7ff472d3c1d0402ee1619fcc01e16b16a5c6
Thadevoos M Biju <116945108+thadevoosm@users.noreply.github.com>
2024-04-25 18:00:39 +0530

    staticparamsV2

--- src/app/blog/[one]/page.tsx
+++ src/app/blog/[one]/page.tsx
@@ -4,7 +4,7 @@
 
 
 async function GetBlog(one: any) {
-    const res = await fetch(`https://661f946f16358961cd94c6d6.mockapi.io/api/v1/Users/${one.one}`);
+    const res = await fetch(`https://661f946f16358961cd94c6d6.mockapi.io/api/v1/Users/${one}`);
     console.log(res.status);
  console.log(one);
 
@@ -16,7 +16,11 @@ async function GetBlog(one: any) {
 }
 
 export async function generateStaticParams(){
-    return [{one:'1'},{one:'2'},{one:'3'},{one:'4'}];
+    const response = await fetch(`https://661f946f16358961cd94c6d6.mockapi.io/api/v1/Users/`).then((res)=> res.json());
+
+    return response.map((resp:any) =>({
+        one: resp.id.toString()
+    }))
 }
 export default  async function BlogDetails({params}:{params: {one: string}}) {
     const blogdet = await GetBlog(params?.one);
